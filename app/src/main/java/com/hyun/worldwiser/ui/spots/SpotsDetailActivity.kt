f82b0878533ddbebe3abfe779d14e896b2e6a41b
hyunho4532 <kyounguk192@gmail.com>
2024-05-23 23:52:09 +0900

    2024-05-23 Haversine 공식을 이용하여 두 지점 사이의 거리 측정 완료

--- app/src/main/java/com/hyun/worldwiser/ui/spots/SpotsDetailActivity.kt
+++ app/src/main/java/com/hyun/worldwiser/ui/spots/SpotsDetailActivity.kt
@@ -1,13 +1,15 @@
 package com.hyun.worldwiser.ui.spots
 
+import android.content.pm.PackageManager
 import android.location.Geocoder
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -17,11 +19,19 @@ import com.hyun.worldwiser.R
 import com.hyun.worldwiser.databinding.ActivitySpotsDetailBinding
 import com.hyun.worldwiser.model.TourSpotsSelect
 import com.hyun.worldwiser.viewmodel.TourSpotsSelectViewModel
+import java.io.IOException
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 class SpotsDetailActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var activitySpotsDetailBinding: ActivitySpotsDetailBinding
     private lateinit var tourSpotsSelectViewModel: TourSpotsSelectViewModel
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,24 +45,69 @@ class SpotsDetailActivity : AppCompatActivity(), OnMapReadyCallback {
         activitySpotsDetailBinding.lifecycleOwner = this
         activitySpotsDetailBinding.tourSpotsSelectViewModel = tourSpotsSelectViewModel
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this )
+
         val tourSpotsTitle = intent.getStringExtra("TourSpotsTitle")
         val tourSpotsAddress = intent.getStringExtra("TourSpotsAddress")
 
         if (tourSpotsTitle != null) {
-            geocodeAddress(tourSpotsTitle, tourSpotsAddress!!, geocoder)
+            geocodeAddress(tourSpotsTitle, tourSpotsAddress!!)
         }
 
         activitySpotsDetailBinding.mapView.onCreate(savedInstanceState)
         activitySpotsDetailBinding.mapView.onResume()
         activitySpotsDetailBinding.mapView.getMapAsync(this)
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+        } else {
+            getLastLocation(geocoder)
+        }
+
+        if (tourSpotsTitle != null && tourSpotsAddress != null) {
+            geocodeAddress(tourSpotsTitle, tourSpotsAddress)
+        }
     }
 
-    private fun geocodeAddress(tourSpotsTitle: String, tourSpotsAddress: String, geocoder: Geocoder) {
+    private fun getLastLocation(geocoder: Geocoder) {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                if (location != null) {
+                    try {
+                        val addresses = geocoder.getFromLocation(location.latitude, location.longitude, 1)
+
+                        if (addresses!!.isNotEmpty()) {
+                            val address = addresses[0].getAddressLine(0)
+                            val currentLat = location.latitude
+                            val currentLng = location.longitude
+
+                            activitySpotsDetailBinding.tvTourSpotsGps.text = address
+
+                            val spotLat = tourSpotsSelectViewModel.tourSpotsLatitude.value
+                            val spotLng = tourSpotsSelectViewModel.tourSpotsLongitude.value
+
+                            if (spotLat != null && spotLng != null) {
+                                val distance = tourSpotsSelectViewModel.setCalculateDistanceSpots(currentLat, currentLng, spotLat, spotLng)
+                                activitySpotsDetailBinding.tvTourSpotsDistance.text = distance.toString()
+                            }
+                        }
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun geocodeAddress(tourSpotsTitle: String, tourSpotsAddress: String) {
+        val geocoder = Geocoder(this)
         val cors = geocoder.getFromLocationName(tourSpotsAddress, 1)
 
-        tourSpotsSelectViewModel.setTourSpots (
-            arrayListOf(TourSpotsSelect(tourSpotsTitle, tourSpotsAddress, cors!![0].latitude, cors[0].longitude))
-        )
+        if (cors!!.isNotEmpty()) {
+            tourSpotsSelectViewModel.setTourSpots (
+                arrayListOf(TourSpotsSelect(tourSpotsTitle, tourSpotsAddress, cors[0].latitude, cors[0].longitude))
+            )
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
--- app/src/main/java/com/hyun/worldwiser/viewmodel/TourSpotsSelectViewModel.kt
+++ app/src/main/java/com/hyun/worldwiser/viewmodel/TourSpotsSelectViewModel.kt
@@ -4,6 +4,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.hyun.worldwiser.model.TourSpotsSelect
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.sqrt
+import kotlin.time.times
 
 
 class TourSpotsSelectViewModel : ViewModel() {
@@ -31,5 +36,23 @@ class TourSpotsSelectViewModel : ViewModel() {
             _tourSpotsLatitude.value = tourSpotsSelect.latitude
             _tourSpotsLongitude.value = tourSpotsSelect.longitude
         }
+    }
+
+    fun setCalculateDistanceSpots(currentLet: Double, currentLng: Double, spotLat: Double, spotLng: Double): Double {
+
+        // 지구 평균 반지름: km
+        val earthRadius = 6371
+
+        val latFromRadians = Math.toRadians(spotLat - currentLet)
+        val lonFromRadians = Math.toRadians(spotLng - currentLng)
+
+        val calculateBetweenTwoPoints =
+            sin(latFromRadians / 2) * sin(latFromRadians / 2) +
+                    cos(Math.toRadians(currentLet)) * cos(Math.toRadians(spotLat)) *
+                    sin(lonFromRadians / 2) * sin(lonFromRadians / 2)
+
+        val calculateDistanceBetweenTwoPoints = 2 * atan2(sqrt(calculateBetweenTwoPoints), sqrt(1 - calculateBetweenTwoPoints))
+
+        return earthRadius * calculateDistanceBetweenTwoPoints
     }
 }
